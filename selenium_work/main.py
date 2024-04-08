c6c9545090436d10247f1f314f4451e39e29bf72
Andrew <1vasilev.andrey@gmail.com>
2024-04-08 09:12:31 +0500

    remove stop function, add alias for specified webdriver

--- selenium_work/main.py
+++ selenium_work/main.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException, WebDriverException
 from time import sleep
-from selenium_work.url.helper import add_protocol
+from url.helper import get_domen_name, add_protocol
 
+DRIVER = webdriver.Chrome
 
 class ScreenshotTaker:
 
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = DRIVER()
 
     def __enter__(self):
         return self
@@ -25,5 +26,33 @@ class ScreenshotTaker:
             except (InvalidArgumentException, WebDriverException):
                 print(f"URL {url} is invalid")
                 return False
+        # self.driver.get(url)
         sleep(sleep_time)
         return self.driver.get_screenshot_as_file(save_path + filename + ".png")
+
+
+urls = [
+    "https://vk.com",
+    "https://youtube.com",
+    "https://rbc.ru",
+    "https://ura.news",
+]
+
+# driver = webdriver.ChromiumEdge()
+# url = urls[0]
+# filename = get_domen_name(url)
+# driver.get(url)
+
+# driver = webdriver.Firefox()
+# for url in urls:
+#     filename = get_domen_name(url)
+#     driver.get(url)
+#     if driver.get_screenshot_as_file("png/" + filename + ".png"):
+#         print(f"Screenshot for {url} was successfully taken!")
+
+# taker.stop()
+with ScreenshotTaker() as taker:
+    for url in urls:
+        filename = get_domen_name(url)
+        if taker.take_from(url, filename, "png/", sleep_time=0):
+            print(f"Screenshot for {url} was successfully taken!")
