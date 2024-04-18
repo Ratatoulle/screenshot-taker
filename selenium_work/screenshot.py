b77992859a80bf088115dda888a9e03b1ea0da7d
Andrew <1vasilev.andrey@gmail.com>
2024-04-18 14:44:20 +0500

    cleanup screenshot.py

--- selenium_work/screenshot.py
+++ selenium_work/screenshot.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException, WebDriverException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from .url.helper import get_domen_name, add_protocol
+from .url.helper import add_protocol
 from time import sleep
 
 DRIVER = webdriver.Chrome
@@ -30,8 +27,6 @@ class ScreenshotTaker:
     def take_from(self, url: str, filename: str, save_path: str, *, sleep_time: int = 10) -> bool:
         try:
             self.driver.get(url)
-            # WebDriverWait(self.driver, sleep_time).until(
-            #     EC.presence_of_element_located((By.ID, "main")))
         except InvalidArgumentException:
             try:
                 url = add_protocol(url)
@@ -41,17 +36,3 @@ class ScreenshotTaker:
                 return False
         sleep(sleep_time)
         return self.driver.get_screenshot_as_file(save_path + filename + ".png")
-
-
-# urls = [
-#     "https://vk.com",
-#     "https://youtube.com",
-#     "https://rbc.ru",
-#     "https://ura.news",
-# ]
-#
-# with ScreenshotTaker() as taker:
-#     for url in urls:
-#         filename = get_domen_name(url)
-#         if taker.take_from(url, filename, "png/", sleep_time=0):
-#             print(f"Screenshot for {url} was successfully taken!")
