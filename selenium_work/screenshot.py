5b345c70cca7a54e0209dd608bf83fbe093b25a6
Andrew <1vasilev.andrey@gmail.com>
2024-04-19 12:44:48 +0500

    minor changes in docker-compose.yml, remove ScreenshotTaker class in screenshot.py

--- selenium_work/screenshot.py
+++ selenium_work/screenshot.py
@@ -6,33 +6,19 @@ from time import sleep
 DRIVER = webdriver.Chrome
 OPTIONS = webdriver.ChromeOptions
 
-
-class ScreenshotTaker:
-
-    def __init__(self):
-        self.options = OPTIONS()
-        self.init_options()
-        self.driver = DRIVER(self.options)
-
-    def init_options(self):
-        self.options.add_argument("--headless=new")
-        self.options.add_argument("--enable-chrome-browser-cloud-management")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.driver.quit()
-
-    def take_from(self, url: str, filename: str, save_path: str, *, sleep_time: int = 10) -> bool:
+def take_from(url: str, *, sleep_time: int = 10) -> bytes | bool:
+    options = OPTIONS()
+    options.add_argument("--headless=new")
+    options.add_argument("--enable-chrome-browser-cloud-management")
+    driver = DRIVER(options)
+    try:
+        driver.get(url)
+    except InvalidArgumentException:
         try:
-            self.driver.get(url)
-        except InvalidArgumentException:
-            try:
-                url = add_protocol(url)
-                self.driver.get(url)
-            except (InvalidArgumentException, WebDriverException):
-                print(f"URL {url} is invalid")
-                return False
-        sleep(sleep_time)
-        return self.driver.get_screenshot_as_file(save_path + filename + ".png")
+            url = add_protocol(url)
+            driver.get(url)
+        except (InvalidArgumentException, WebDriverException):
+            print(f"URL {url} is invalid")
+            return False
+    sleep(sleep_time)
+    return driver.get_screenshot_as_png()
