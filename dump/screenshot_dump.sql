49758be9084d53099dfb07f5b732056fef14d4c7
Andrew <1vasilev.andrey@gmail.com>
2024-04-19 13:42:59 +0500

    add dump folder

---
+++ dump/screenshot_dump.sql
@@ -0,0 +1,28 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+
+CREATE TABLE public.url_path (
+    url text,
+    s3_path text
+);
+
+
+ALTER TABLE public.url_path OWNER TO root;
+
+
+COPY public.url_path (url, s3_path) FROM stdin;
+\.
+
